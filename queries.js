0133f399e6f294831d0c4a36b9ff84e8111796ba
Jordan Heffernan <jheff2@gmail.com>
2019-01-02 09:39:50 -0800

    6 and 7

--- queries.js
+++ queries.js
@@ -18,41 +18,62 @@ const printResults = function(results) {
   console.log(results)
 };
 
-knex
-.select().table('restaurants')
-.then(results=> {
-  console.log(results)
-});
+// //1
+
+// knex
+// .select().table('restaurants')
+// .then(results=> {
+//   console.log(results)
+// });
+
+// //2
+
+// knex
+//   .select().from('restaurants')
+//   .where({ cuisine: 'Italian'})
+//   .debug(true)
+//   .then(results => console.log(results))
+
+// // 3
+
+// knex
+//   .select('id', 'name')
+//   .from('restaurants')
+//   .where( {cuisine: 'Italian'})
+//   .limit(10)
+//   .debug(true)
+//   .then(printResults)
+
+//   // 4
+
+//   knex 
+//   .select()
+//   .count()
+//   .from('restaurants')
+//   .where({cuisine: 'Thai'})
+//   .then(printResults)
+
+//   // 5
+
+//   knex
+//   .select()
+//   .count()
+//   .from('restaurants')
+//   .then(printResults)
+
+// 6
+
+// knex 
+//   .select()
+//   .from('restaurants')
+//   .where({address_zipcode: '11372', cuisine: 'Thai'})
+//   .then(printResults)
+
+// 7
 
 knex
-  .select().from('restaurants')
-  .where({ cuisine: 'Italian'})
-  .debug(true)
-  .then(results => console.log(results))
-
-knex
-  .select('id', 'name')
+  .select('id', 'name', 'address_zipcode')
   .from('restaurants')
-  .where( {cuisine: 'Italian'})
-  .limit(10)
-  .debug(true)
+  .whereIn('address_zipcode', ['10012', '10013', '10014'])
+  .andWhere({cuisine: 'Italian'})
   .then(printResults)
-
-  // 4
-
-  knex 
-  .select()
-  .count()
-  .from('restaurants')
-  .where({cuisine: 'Thai'})
-  .then(printResults)
-
-  // 5
-
-  knex
-  .select()
-  .count()
-  .from('restaurants')
-  .then(printResults)
-
-  
