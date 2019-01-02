1bbbda4fde2849768ed237f2c76b941a6571c69c
Marwan Alaloosi <manoalosi@gmail.com>
2019-01-02 10:56:39 -0600

    nothing yet

--- queries.js
+++ queries.js
@@ -4,9 +4,18 @@ const { DATABASE } = require('./config');
 const knex = require('knex')(DATABASE);
 
 // Sample select 
+
+
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+
 knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
+.select().table('restaurants')
+.then(results=> {
+  console.log(results)
+});
+
